2f4dd6b6f6df21fb1e232d2714f21fd34163ebd5
Hebert Soares <hebertsoaresof@gmail.com>
2022-11-22 19:43:37 -0300

    Programa Completo

---
+++ Programa Pet (JAVA - POO)/src/Animais/Main.java
@@ -0,0 +1,9 @@
+package Animais;
+public class Main {
+    public static void main(String[] args) {
+        Cachorro x = new Cachorro();
+        x.emitirSom();
+        x.reagir(false);
+    }
+    
+}
